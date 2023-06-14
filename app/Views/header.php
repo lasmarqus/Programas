a847451903ac3699d4cec149a38c1b6d102f8715
Laís Cristina <104374989+lasmarqus@users.noreply.github.com>
2023-06-14 16:34:57 -0400

    Update header.php
    alteração da cor dos botões para verde

--- app/Views/header.php
+++ app/Views/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-sm navbar-dark">
             <a class="navbar-brand" href="<?=URL?>">LOGO</a>
-            <button class="btn btn-sucess" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -16,8 +16,8 @@
                     </li>
                 </ul>
                                 <span class="navbar-text">
-                    <a class="btn btn-info" href="#" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
-                    <a class="btn btn-info" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
+                    <a class="btn btn-success" href="#" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
+                    <a class="btn btn-success" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
                 </span>
                             </div>
         </nav>

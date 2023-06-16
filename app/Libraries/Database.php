c716c5008f70aaacca6cca9bdcfd46498d5e2708
Laís Cristina <104374989+lasmarqus@users.noreply.github.com>
2023-06-15 20:29:25 -0400

    Update Database.php
    correção de error

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -1,9 +1,9 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-    $dbh = null;
+ $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+ $dbh = null;
 } catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+ print "Error!: " . $e->getMessage() . "<br/>";
+ die();
 }
 ?>
